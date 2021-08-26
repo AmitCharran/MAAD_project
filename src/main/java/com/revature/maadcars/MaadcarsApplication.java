0028e4835827d8faabb047e92dec8a571172a356
Alex Nowak <anowak2692@gmail.com>
2021-08-26 10:59:58 -0500

    Initial commit

---
+++ src/main/java/com/revature/maadcars/MaadcarsApplication.java
@@ -0,0 +1,13 @@
+package com.revature.maadcars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaadcarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaadcarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/maadcars/MaadcarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.maadcars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaadcarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
