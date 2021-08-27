9c7defaaab091872d98746346117d4f4d3ec3851
DDRIV <ddriv4@gmail.com>
2021-08-27 15:29:24 -0500

    Added Vehicle controller/service test classes

---
+++ src/test/java/com/revature/maadcars/controllers/VehicleControllerTest.java
@@ -0,0 +1,33 @@
+package com.revature.maadcars.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VehicleControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAllVehicles() {
+    }
+
+    @Test
+    void findVehicleById() {
+    }
+
+    @Test
+    void createVehicle() {
+    }
+
+    @Test
+    void updateVehicle() {
+    }
+
+    @Test
+    void deleteVehicle() {
+    }
+}
